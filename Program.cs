2e487f8c8842bc72b6500feda37254d8b7e6e7f5
elandeol1969 <elias.antoniol@gmail.com>
2023-07-31 12:21:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeuApp
+
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alô, Mundo!");
+        }
+    }
+}
